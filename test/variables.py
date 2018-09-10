a9152888af36e569436393b0520cd9745950bf1c
crystalDf <chendong333@gmail.com>
2018-09-11 03:21:54 +0800

    Chapter 01 guess number

--- test/variables.py
+++ test/variables.py
@@ -2,3 +2,6 @@ userAge, userName = 30, 'Peter'
 print(userAge)
 print(userName)
 print(userAge + userName)
+
+message= 'The price of this {} laptop is {} USD and the exchange rate is {} USD to 1 EUR'\
+    .format('Apple', 1299, 1.235235245)
