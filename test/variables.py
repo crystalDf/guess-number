303f973e050418715771aa6c7c0ad7ed100e2331
crystalDf <chendong333@gmail.com>
2018-09-12 01:19:23 +0800

    Chapter 01 guess number

--- test/variables.py
+++ test/variables.py
@@ -1,7 +1,12 @@
 userAge, userName = 30, 'Peter'
 print(userAge)
 print(userName)
-print(userAge + userName)
+print(str(userAge) + userName)
 
-message= 'The price of this {} laptop is {} USD and the exchange rate is {} USD to 1 EUR'\
+message = 'The price of this {} laptop is {} USD and the exchange rate is {} USD to 1 EUR'\
     .format('Apple', 1299, 1.235235245)
+
+message = 'The price of this {0:s} laptop is {1:d} USD and the exchange rate is {2:4.2f} USD to 1 EUR'\
+    .format('Apple', 1299, 1.235235245)
+
+print(message)
